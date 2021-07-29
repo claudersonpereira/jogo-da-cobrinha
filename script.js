d4f5402845b9f320a34d082ecb907bacc2c8a001
claudersonsilva <clauderson.silva@hotmail.com>
2021-07-29 10:41:41 -0300

    criar comida

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ snake[0] = {
 
 let direction = "right";
 
+let food = {
+    x : Math.floor(Math.random() * 15 + 1) * box,
+    y : Math.floor(Math.random() * 15 + 1) * box
+}
+
+
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -20,6 +26,12 @@ function criarCobrinha(){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }    
+}
+
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -40,6 +52,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x; 
     let snakeY = snake[0].y;
